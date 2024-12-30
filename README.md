8fa803c51b2dfd2ccd7add20b4dfc374f01beb14
Neuzzy <173589117+neuzzz3628@users.noreply.github.com>
2024-12-30 03:03:08 +0000

    Make README, html and javascript (Computer-choice)
    Add descs in README, make html to project to browser's console, create a function that generates a random choice for computer

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # odin-rps
+This is a TOP project about a Rock-Paper-Scissors game.
+
+How does it work?
+1. There are a total of 5 rounds.
+2. Each round, the player inserts their input which is compared against the computer's.
+3. The scores will be logged for each round.
---
+++ javascript.js
@@ -0,0 +1,19 @@
+// console.log("Hello World")
+
+// Method 1: using Array
+// Create a function that randomly generates a choice between Rock, Paper and Scissors.
+// Use an array to contain all possible choices, then use Math random to generate an index number to be returned.
+// The array has 3 values, so the index must return either of these numbers (0, 1, 2).
+// function getComputerChoice(){
+//     const choices = ["Rock", "Paper", "Scissors"];
+//     let randomChoiceIndex = Math.floor(Math.random() * 3);
+//     return choices[randomChoiceIndex];
+// }
+
+// Method 2: using If-Else
+// With IF-Else, make conditions with Math random that returns a certain choice (either Rock, Paper or Scissors)
+
+function getComputerChoice(){
+    let randomChoiceNum = Math.floor(Math.random()*3) + 1;
+    return (randomChoiceNum === 1) ? "Rock" : (randomChoiceNum === 2) ? "Paper" : "Scissors"
+}
